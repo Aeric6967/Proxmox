4a4c384866fb7f4c7d880b329c69c8224feacf70
Eric <eh10597@gmail.com>
2023-08-24 20:58:01 -0500

    Updating OS version and disk size in octoprint.sh

--- ct/octoprint.sh
+++ ct/octoprint.sh
@@ -19,11 +19,11 @@ EOF
 header_info
 echo -e "Loading..."
 APP="OctoPrint"
-var_disk="4"
+var_disk="8"
 var_cpu="1"
 var_ram="1024"
 var_os="debian"
-var_version="11"
+var_version="12"
 variables
 color
 catch_errors
